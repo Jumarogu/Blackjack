06a25333856d349ee03d94f06650c3d65136bec0
Juan Manuel Romero Guardado <jmromerog511@gmail.com>
2015-05-02 17:50:22 -0500

    Naipe
    naipe class

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hermes marica");
-		System.out.println("Hola");
-
+		//System.out.println("Hermes marica");
+		//System.out.println("Hola");
+		Naipe n = new Naipe(0, 8);
+		System.out.println(n.getValor());
+		System.out.println(n.toString());
 	}
 
 }
